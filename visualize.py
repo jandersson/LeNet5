f56e3d7f25990ef66a6ae065252f96f2f03b8f8b
jandersson <jonas@andersson.io>
2018-04-30 14:42:37 +0200

    Plot weight distributions for all layers

--- visualize.py
+++ visualize.py
@@ -6,7 +6,10 @@ import numpy as np
 class Visualizer:
     def __init__(self):
         self.vis = visdom.Visdom()
-        assert self.vis.check_connection()
+        try:
+            self.vis.check_connection()
+        except ConnectionError:
+            print("Visdom may not be running. Run 'python -m visdom.server' if it is not.")
 
         # Visdom requires one data point for initializing a plot. No blankboards allowed yet.
         self.train_loss_plot = None
@@ -44,6 +47,7 @@ class Visualizer:
                           win=self.test_accuracy_plot,
                           update='append')
 
+
     def write_log(self, message):
         print(message)
         if not self.text_log:
@@ -53,3 +57,18 @@ class Visualizer:
             self.log_messages = self.log_messages + f"\n<br>{message}"
             self.vis.text(self.log_messages, win=self.text_log)
 
+    def plot_weight_dist(self, state_dict):
+        """Take a dictionary from a saved model and plot the weight distribution for all weights for all layers"""
+        for layer_name, params in state_dict.items():
+            if 'bias' in layer_name:
+                continue
+
+            self.vis.histogram(params.view(params.numel()),
+                               opts=dict(numbins=40,
+                                         title=f"{layer_name} Weight Distribution"))
+
+if __name__ == '__main__':
+    import torch
+    state = torch.load('checkpoint.pth.tar')['state_dict']
+    v = Visualizer()
+    v.plot_weight_dist(state)
